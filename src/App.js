26e20c4a16233705c49d4c0dfca415c850a5547a
Fatemeh Yeganeh <fatemeh.code@gmail.com>
2022-11-13 15:26:30 +0100

    Query Hook gt data from api and control refetch

--- src/App.js
+++ src/App.js
@@ -10,26 +10,35 @@ import {Blog} from './Pages/Blog/Blog';
 import {UsingAPIs} from "./Pages/UsingAPIs";
 import {Home} from './Pages/Home';
 import { TopComponent } from "./Pages/useContextSample/TopComponent";
-
+import{QueryClient,QueryClientProvider}from '@tanstack/react-query';
 export const AppContext=createContext();
 function App() {
   const [username,setUsername]=useState('Fahimeh');
+  const client=new QueryClient({defaultOptions:{
+    queries:{
+      refetchOnWindowFocus:false,
+      //refetchInterval:1000,
+    }
+  }});
   return (
     <div className="App">     
-      <AppContext.Provider value={{username}}>      
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/ToDo" element={<ToDo/>} />
-            <Route path="/Blog" element={<Blog/>} />          
-            <Route path="/UsingAPIs" element={<UsingAPIs/>}/>
-            <Route path="/TopComponent" element={<TopComponent/>}/>
-            <Route path="*" element={<h1>Page not Found</h1>}/>
+      <AppContext.Provider value={{username}}> 
+        <QueryClientProvider client={client}>
+             
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/ToDo" element={<ToDo/>} />
+              <Route path="/Blog" element={<Blog/>} />          
+              <Route path="/UsingAPIs" element={<UsingAPIs/>}/>
+              <Route path="/TopComponent" element={<TopComponent/>}/>
+              <Route path="*" element={<h1>Page not Found</h1>}/>
 
-          </Routes>          
-        </Router>      
-        <Footer/>
+            </Routes>          
+          </Router>      
+          <Footer/>
+        </QueryClientProvider>
       </AppContext.Provider>    
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,19 @@
 import { useContext } from "react";
+import Axios from "axios";
 import { AppContext } from "../App";
+import{useQuery} from "@tanstack/react-query";
 export const Home=()=>{
+    const {data,isLoading,isError,refetch}=useQuery(["cat"],()=>{
+        return(Axios.get("https://catfact.ninja/fact").then((res)=>res.data));            
+    });
     const {username} =useContext(AppContext);
-    return  <h1>Home {username}</h1>
+    if(isLoading){return (<h1>Loading...</h1>);}
+    if(isError){return(<h1>Error...</h1>);}
+    return ( 
+        <div>
+            <h1>HomePage user: {username}</h1>
+            <h3>{data?.fact}</h3>
+            <button onClick={refetch}>Update</button>
+        </div>
+    );
 }
